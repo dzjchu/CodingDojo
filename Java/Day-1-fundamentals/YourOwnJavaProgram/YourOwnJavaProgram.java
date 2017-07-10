a55bfddde1f06a4d316a88d9382f86af173af9f1
Code <code@Dans-MacBook-Pro.local>
2017-07-10 10:46:07 -0700

    First Java and Print out something from java

---
+++ Java/Day-1-fundamentals/YourOwnJavaProgram/YourOwnJavaProgram.java
@@ -0,0 +1,7 @@
+public class YourOwnJavaProgram {
+  public static void main(String[] args){
+    System.out.println("My name is Dan Chu");
+    System.out.println("I am 26 years old");
+    System.out.println("My hometown is New York, New York");
+  }
+}
