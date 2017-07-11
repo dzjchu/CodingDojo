fb3faed526bdcb80f7979120143f066f222461c4
Code <code@Dans-MacBook-Pro.local>
2017-07-10 19:51:12 -0700

    Finished BasicJava, Contiune PuzzleJava

---
+++ Java/Day-1-fundamentals/puzzling/PuzzleJava.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class PuzzleJava{
+  public static void main(String[] args) {
+
+    //Create an array with the following values: 3,5,1,2,7,9,8,13,25,32. Print the sum of all numbers in the array. Also have the function return an array that only includes numbers that are greater than 10 (e.g. when you pass the array above, it should return an array with the values of 13,25,32)
+    // int[] myArr = {3,5,1,2,7,9,8,13,25,32};
+    // ArrayList<Integer> MyArrList = new ArrayList<>();
+    // int sum = 0;
+    // for (int i = 0; i < myArr.length; i++){
+    //   sum += myArr[i];
+    //   if (myArr[i]>10){
+    //     MyArrList.add(myArr[i]);
+    //   }
+    // }
+    // System.out.println(sum);
+    // System.out.println(MyArrList);
+
+
+    //Create an array with the following values: Nancy, Jinichi, Fujibayashi, Momochi, Ishikawa. Shuffle the array and print the name of each person. Have the method also return an array with names that are longer than 5 characters.
+    // String[] Strs = {"Nancy", "Jinichi", "Fujibayashi", "Momochi", "Ishikawa"};
+    // ArrayList<String> myArrList = new ArrayList<String>(Arrays.asList(Strs));
+    // ArrayList<String> newArrList = new ArrayList<>();
+    // System.out.println(myArrList);
+    // Collections.shuffle(myArrList);
+    // System.out.println(myArrList);
+    // for (String name: myArrList){
+    //   if (name.length() > 5){
+    //     newArrList.add(name);
+    //   }
+    // }
+    // System.out.println(newArrList);
+
+    //Create an array that contains all 26 letters in the alphabet (this array must have 26 values). Shuffle the array and display the last letter of the array. Have it also display the first letter of the array. If the first letter in the array is a vowel, have it display a message
+
+
+
+  }
+}
